569a7fb56e222612493420c2a6b01227e6c09a2a
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2024-11-15 22:33:25 +0700

    Init Frontend

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SafePocket
+# SmartPocket
---
+++ frontend/src/lib/constants/index.ts
@@ -0,0 +1,10 @@
+// Metadata Head
+export const metadata = {
+  title: 'SmartPocket',
+  description: 'SmartPocket',
+  keywords: 'SmartPocket',
+  type: 'website',
+  url: '',
+  image: '',
+  autor: '',
+};
