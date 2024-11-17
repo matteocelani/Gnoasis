8c7991559142d542acb19270d3f5afe90d291038
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2024-11-17 08:36:30 +0700

    webmanifest

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,21 @@ import withPWA from 'next-pwa';
 const nextConfig = {
   reactStrictMode: true,
   ssr: true,
-  serverRuntimeConfig: {
-    wallet_connect_id: process.env.NEXT_PUBLIC_WALLETCONNECT_ID,
-    dune_api_key: process.env.NEXT_PUBLIC_DUNE_API_KEY,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ico|png|svg|jpg|jpeg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            publicPath: '/_next/static/images/',
+            outputPath: 'static/images/',
+          },
+        },
+      ],
+    });
+    return config;
   },
 };
 
